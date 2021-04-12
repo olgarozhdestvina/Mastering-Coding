b39383021817464f6f7eaeeaeaf9b48141e8685f
olgarozhdestvina <olgarozhdestvina@gmail.com>
2021-04-12 11:16:08 +0800

    HTML: replaced images and links, added links to social media and github projects. Also added favicon icon. Server: added error handlers

--- Web_Development/server.py
+++ Web_Development/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from werkzeug.exceptions import BadRequest, NotFound, MethodNotAllowed, InternalServerError
 import os
 
 app = Flask(__name__, template_folder=os.getcwd(), static_folder='assets')
@@ -7,6 +8,25 @@ app = Flask(__name__, template_folder=os.getcwd(), static_folder='assets')
 def index():
     return render_template('index.html')
 
-@app.route('/blog')
-def blog():
-    return 'Blog blah blah'
+# Error handlers.
+@app.errorhandler(BadRequest)
+def handle_bad_request(e):
+    return 'Bad request!', 400
+
+
+@app.errorhandler(NotFound)
+def handle_bad_request1(e):
+    return 'Page is not found!', 404
+
+
+@app.errorhandler(MethodNotAllowed)
+def handle_bad_request2(e):
+    return 'Method is not allowed!', 405
+
+
+@app.errorhandler(InternalServerError)
+def handle_bad_request3(e):
+    return 'Internal server error!', 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
