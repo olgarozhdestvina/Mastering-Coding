700a8fa408bd6bfdb54710bc00beede0b7412ac6
olgarozhdestvina <olgarozhdestvina@gmail.com>
2021-05-24 12:49:12 +0800

    Started working on tkk widgets (buttons and labels)

---
+++ GUI Development with Tkinter/tkk_button.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+
+# Create a window and add a button to it
+root = Tk()
+
+button = ttk.Button(root, text = 'Click me')
+button.pack()
+
+# Button text property
+print(button['text'])
+button['text'] = 'Press me'
+button.config(text = 'Push me')
+
+# All button properties
+print(button.config())
+
+root.mainloop()
+
+# Underlying Tk names for root and button
+print(root)
+print(button)
